c443ea2bc39866e00728e9878c91177f237dee15
Aashay Wadkar <aashay.0001@gmail.com>
2021-11-10 21:06:06 +0530

    Update GabsApplication.java

--- src/main/java/com/psl/training/config/GabsApplication.java
+++ src/main/java/com/psl/training/config/GabsApplication.java
@@ -2,8 +2,12 @@ package com.psl.training.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.psl.training"})
 public class GabsApplication {
 
 	public static void main(String[] args) {
