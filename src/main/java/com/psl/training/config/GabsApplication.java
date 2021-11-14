9e4530bd5ea773f17a8350e82740f58d39ae5eeb
Aashay Wadkar <aashay.0001@gmail.com>
2021-11-14 11:46:55 +0530

    Update GabsApplication.java

--- src/main/java/com/psl/training/config/GabsApplication.java
+++ src/main/java/com/psl/training/config/GabsApplication.java
@@ -3,11 +3,16 @@ package com.psl.training.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 @EntityScan(basePackages = {"com.psl.training"})
+@ComponentScan(basePackages = {"com.psl.training"})
+@EnableJpaRepositories(basePackages = {"com.psl.training"})
+@EnableConfigurationProperties
 public class GabsApplication {
 
 	public static void main(String[] args) {
