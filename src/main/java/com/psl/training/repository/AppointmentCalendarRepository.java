836ea6f98c4050834c1be1872b8994da11fbc659
Aashay Wadkar <aashay.0001@gmail.com>
2021-11-14 11:54:04 +0530

    Update AppointmentCalendarRepository.java

--- src/main/java/com/psl/training/repository/AppointmentCalendarRepository.java
+++ src/main/java/com/psl/training/repository/AppointmentCalendarRepository.java
@@ -5,14 +5,15 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.psl.training.entity.AppointmentCalendar;
+import com.psl.training.entity.User;
 
 public interface AppointmentCalendarRepository  extends JpaRepository<AppointmentCalendar, Long> {
 
-	public List<AppointmentCalendar> findByuserName(String userName);
+
 	
 	public List<AppointmentCalendar> findByType(String type);
 
 	public List<AppointmentCalendar> findByLocation(String location);
 	
-	public List<AppointmentCalendar> findByUserID(long userId);
+	public List<AppointmentCalendar> findByOwner(User owner);
 }
